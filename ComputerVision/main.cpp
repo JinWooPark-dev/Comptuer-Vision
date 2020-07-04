2364c0c76f944e1f985ad8367380bc7778842b3a
jwPark <pjw9206@naver.com>
2020-07-04 16:07:26 +0900

    init

---
+++ ComputerVision/main.cpp
@@ -0,0 +1,21 @@
+/**
+	@mainpage	Computer Vison
+	@brief		main function
+
+	@section	detail Detailed Description
+	@details	This program is about computer vision.
+
+	@section	author Author Information
+	@author		Jinwoo Park
+	@date		2020-07-04
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Test"<<endl;
+
+	return 0;
+}
