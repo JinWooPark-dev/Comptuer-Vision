dcd709ffde3acd485411b78214d919c8dc08d32f
jwPark <pjw9206@naver.com>
2020-07-08 13:22:31 +0900

    Install openCV API

--- ComputerVision/main.cpp
+++ ComputerVision/main.cpp
@@ -10,12 +10,20 @@
 	@date		2020-07-04
 */
 #include <iostream>
+#include "opencv.hpp"
+#include "core/core.hpp"
+#include "highgui/highgui.hpp"
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
-	cout<<"Test"<<endl;
+	Mat test = imread("E:\\project\\ComputerVision\\inputImage\\001.jpg", 1);
+
+	imshow("test", test);
+	waitKey(0);
+	
 
 	return 0;
 }
